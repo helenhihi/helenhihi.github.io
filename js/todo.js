8fa3d147e043d7fa5cd1501210cb2b9ab4043536
helenhihi <hrparka@naver.com>
2022-01-24 02:25:13 +0900

    Create project

--- js/todo.js
+++ js/todo.js
@@ -22,8 +22,10 @@ function paintToDo(newTodo) {
   li.id = newTodo.id;
   const span = document.createElement("span");
   span.innerText = newTodo.text;
+  span.classList.add("todo-font");
   const button = document.createElement("button");
   button.innerText = "❌";
+  button.classList.add("delete-button");
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);

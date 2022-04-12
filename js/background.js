5a9e8d0769f6cea49ae965a93bd378258edc1fc5
hrparkdev <hrparkdev@gmail.com>
2022-04-12 15:26:03 +0900

    Delete all

--- js/background.js
+++
@@ -1,9 +0,0 @@
-const body = document.body;
-
-const images = ["0.jpg", "1.jpg", "2.jpg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-body.style.backgroundImage = `url(img/${chosenImage})`;
-
-body.style.backgroundSize = "cover";
--- js/calendar.js
+++
@@ -1,5 +0,0 @@
-const calendar = document.querySelector("h2#calendar");
-
-const date = new Date();
-const today = date.toDateString();
-calendar.innerText = today;
